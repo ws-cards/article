463761f6f82fb1ba4675675e71a4f2833fa6a142
ws-cards <72481141+ws-cards@users.noreply.github.com>
2020-12-16 01:34:26 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function handleSuccess(stream) {
   const video = document.querySelector('video');
   const videoTracks = stream.getVideoTracks();
   console.log('Got stream with constraints:', constraints);
-  console.log(`Using video device: ${videoTracks[0].label}`);
+  console.log('Using video device: ${videoTracks[0].label}');
   window.stream = stream; // make variable available to browser console
   video.srcObject = stream;
 }
@@ -25,18 +25,18 @@ function handleSuccess(stream) {
 function handleError(error) {
   if (error.name === 'ConstraintNotSatisfiedError') {
     const v = constraints.video;
-    errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
+    errorMsg('The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.');
   } else if (error.name === 'PermissionDeniedError') {
     errorMsg('Permissions have not been granted to use your camera and ' +
       'microphone, you need to allow the page access to your devices in ' +
       'order for the demo to work.');
   }
-  errorMsg(`getUserMedia error: ${error.name}`, error);
+  errorMsg('getUserMedia error: ${error.name}', error);
 }
 
 function errorMsg(msg, error) {
   const errorElement = document.querySelector('#errorMsg');
-  errorElement.innerHTML += `<p>${msg}</p>`;
+  errorElement.innerHTML += '<p>${msg}</p>';
   if (typeof error !== 'undefined') {
     console.error(error);
   }
